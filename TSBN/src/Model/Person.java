8a7858085467dad86887c32db29d07863a9f5017
sapirani081 <65303268+sapirani081@users.noreply.github.com>
2020-07-01 23:40:06 +0300

    classes

---
+++ TSBN/src/Model/Person.java
@@ -0,0 +1,24 @@
+package Model;
+
+abstract class  Person{
+    private  int ID;
+    private  String last_name;
+    private  String first_name;
+    private String mail;
+    // constructor
+    public Person(String last_name,String first_name,int ID,String mail) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.ID = ID;
+        this.mail=mail;
+    }
+    public String getLast_name() { return last_name; }
+    public void setLast_name(String last_name) { this.last_name = last_name; }
+    public String getFirst_name() { return first_name; }
+    public void setFirst_name(String first_name) { this.first_name = first_name; }
+    public String getMail() { return mail; }
+    public void setMail(String mail) { this.mail = mail; }
+    public int getIDperson(){
+        return this.ID;
+    }
+}
---
+++ TSBN/src/Model/Worker.java
@@ -0,0 +1,72 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Worker extends Person {
+    int NUM_WORKER = 1;
+
+    public Worker(String last_name, String first_name, int ID, String mail,int department_id, int Job_ID, float experience, float base_salary,String user_name, String password) {
+        super(last_name, first_name, ID,mail);
+        this.num_Worker = NUM_WORKER++;
+        this.department_ID = department_id;
+        this.experience = experience;
+        this.base_Salary = base_salary;
+        this.hours = 0;
+        this.assessment = new ArrayList<String>();
+        this.user_name=user_name;
+        this.password = password;
+        this.job_ID = job_ID;
+    }
+
+
+
+    private int num_Worker;
+    private int department_ID;
+    private double experience;
+    private double base_Salary;
+    private double hours;
+    private ArrayList<String> assessment;
+    private String user_name;
+    private String password;
+    private int job_ID;
+
+    public int getNum_Worker() { return num_Worker; }
+    public int getDepartment_ID() { return department_ID; }
+    public double getExperience() { return experience; }
+    public double getBase_Salary() { return base_Salary; }
+    public double getHours() { return hours; }
+    public ArrayList<String> getAssessment() { return assessment; }
+    public String getPassword() { return password; }
+    public String getUser_name(){ return user_name;}
+    public int getJob_ID() { return job_ID; }
+    public void setHours(float hours) { this.hours = hours; }
+    void changeJobID(int new_ID){
+        this.job_ID=new_ID;
+    }
+    void changeExperience(float new_experience){
+        this.experience=new_experience;
+    }
+    void changeDepartmentID(int new_department){
+        this.department_ID=new_department;
+    }
+    void changeSalary(float new_salary){
+        this.base_Salary=new_salary;
+    }
+    double calculateSalary(){
+        return base_Salary*hours;
+    }
+    void addAssessment(String assessment1){
+        assessment.add( assessment1);
+    }
+    boolean changePassword(String new_pass) {
+        if (new_pass.length() > 8) {
+            this.password = new_pass;
+            return true;
+        }
+        return false;
+
+    }
+
+
+}
+
