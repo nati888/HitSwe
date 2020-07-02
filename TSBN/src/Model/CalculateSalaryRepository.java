554ca3b56d43e1f9691c04bcf8d06b45b4e7067f
sapir <sapirani081@gmail.com>
2020-07-02 12:40:10 +0300

    Repository

---
+++ TSBN/src/Model/CalculateSalaryRepository.java
@@ -0,0 +1,27 @@
+package Model;
+
+import java.io.IOException;
+        import java.util.ArrayList;
+        import java.util.UUID;
+
+public class CalculateSalaryRepository {
+
+    private static CalculateSalaryRepository Instance;
+    private CalculateSalaryRepository() { }
+
+    public static CalculateSalaryRepository getMySingelton(){
+        if(Instance==null)
+            Instance=new CalculateSalaryRepository();
+        return Instance;
+    }
+
+    public String calculateSalary(String ID) throws IOException {
+        Workers worker_list=Workers.getMySingelton();
+        Worker worker=worker_list.searchingWorker(Integer.parseInt(ID));
+        if (worker==null)
+            return null;
+        worker_list.removeWorker(worker);
+        return UUID.randomUUID().toString();
+    }
+
+}
